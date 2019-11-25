2187959437ce816718d13f008c43751557213dd2
Samantha Smith <smith.samantha.lynn@gmail.com>
2019-11-25 17:49:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 var kitten = "Ralph"
 
-function destructivelyAppendKitten(kittens){
-  return [kittens, kitten];
+function destructivelyAppendKitten(kittens, kitten){
+  kittens.unshift('Ralph')
+  return kittens;
 }
 
 // Add your functions and code here
